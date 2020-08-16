510d741bde217c18d66fea909e6174657834f56f
Aditi Thakur <a.thakur73.at@gmail.com>
2020-08-16 14:48:55 +0530

    added logic to validate full url

--- app/models/short_url.rb
+++ app/models/short_url.rb
@@ -2,7 +2,9 @@ class ShortUrl < ApplicationRecord
 
   CHARACTERS = [*'0'..'9', *'a'..'z', *'A'..'Z'].freeze
 
+  validates_presence_of :full_url
   validate :validate_full_url
+  validates_uniqueness_of :short_code, :full_url
 
   def short_code
   end
@@ -13,6 +15,14 @@ class ShortUrl < ApplicationRecord
   private
 
   def validate_full_url
+    if self.full_url.present?
+      uri = URI.parse(URI.encode(self.full_url.strip))
+      unless uri.is_a?(URI::HTTP) && !uri.host.nil?
+        errors.add(:full_url, "is not a valid url")
+      end
+    else
+      errors.add(:full_url, "can't be blank")
+    end
   end
 
 end
