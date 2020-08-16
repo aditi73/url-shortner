1ab237f9d3b63bddf63ea57073e20abba4002d6c
Aditi Thakur <a.thakur73.at@gmail.com>
2020-08-16 14:42:59 +0530

    added database fields for short urls

--- db/migrate/20190923133432_create_short_urls.rb
+++ db/migrate/20190923133432_create_short_urls.rb
@@ -2,7 +2,10 @@ class CreateShortUrls < ActiveRecord::Migration[6.0]
   def change
     create_table :short_urls do |t|
       # You'll want to add some attributes here...
-
+      t.string :full_url #long_url
+      t.string :short_code
+      t.string :title
+      t.integer :click_count, default: 0
       t.timestamps
     end
   end
